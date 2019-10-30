58c44075fffdbe580239dbebc2f78e692f576271
prongbang <prongbang@gmail.com>
2019-10-30 17:49:10 +0700

    migrate to androidx

--- app/src/main/java/com/prongbang/barcode128/MainActivity.java
+++ app/src/main/java/com/prongbang/barcode128/MainActivity.java
@@ -1,8 +1,8 @@
 package com.prongbang.barcode128;
 
 import android.graphics.Bitmap;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -18,12 +18,12 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        ivBarcode128 = (ImageView) findViewById(R.id.ivBarcode128);
-        tvCode = (TextView) findViewById(R.id.tvCode);
+        ivBarcode128 = findViewById(R.id.ivBarcode128);
+        tvCode = findViewById(R.id.tvCode);
 
         String code = "1234567891013";
         Bitmap bitmap = BarcodeEncoder.genBarcode128(code, 550, 222);
-        if(bitmap != null) {
+        if (bitmap != null) {
             ivBarcode128.setImageBitmap(bitmap);
             tvCode.setText(code);
         }
